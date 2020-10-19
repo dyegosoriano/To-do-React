f6139bcf9791b9b49f7b371a2afa757951b02239
Dyego Soriano <dyegodf2@gmail.com>
2020-10-19 20:55:38 -0300

    criando botão de salvat nova todo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,60 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { Alert, KeyboardAvoidingView } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 import { todosContext } from '../../contexts/Todos'
 import BoxTodo from '../../components/BoxTodo'
 
-import { Container, TodoList } from './styles'
+import {
+  BoxAddNewTodo,
+  ButtonNewTodo,
+  Container,
+  DescriptionTodo,
+  TodoList
+} from './styles'
 
 const Home: React.FC = () => {
-  const { todos } = useContext(todosContext)
+  const { todos, addNewTodo } = useContext(todosContext)
+
+  const [description, setDescription] = useState('')
+
+  function handlePressButton() {
+    if (!description) {
+      Alert.alert(
+        'Ops!!!',
+        'Você precisa inserir uma descrição para criar uma nova tarefa'
+      )
+      return
+    }
+
+    addNewTodo(description)
+    setDescription('')
+  }
 
   return (
-    <Container>
-      <TodoList alwaysBounceHorizontal={false}>
-        {todos.map(item => (
-          <BoxTodo key={item.id} todo={item} />
-        ))}
-      </TodoList>
-    </Container>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+    >
+      <Container>
+        <TodoList alwaysBounceHorizontal={false}>
+          {todos.map(item => (
+            <BoxTodo key={item.id} todo={item} />
+          ))}
+        </TodoList>
+
+        <BoxAddNewTodo>
+          <DescriptionTodo
+            placeholder="Insira aqui uma nova tarefa."
+            onChangeText={text => setDescription(text)}
+            value={description}
+          />
+
+          <ButtonNewTodo onPress={handlePressButton}>
+            <Feather name="plus" size={24} color="#fff" />
+          </ButtonNewTodo>
+        </BoxAddNewTodo>
+      </Container>
+    </KeyboardAvoidingView>
   )
 }
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,11 +3,34 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   margin: 16px;
 
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  height: 97%;
+  justify-content: space-between;
 `
 
 export const TodoList = styled.ScrollView`
   width: 100%;
+`
+export const BoxAddNewTodo = styled.View`
+  border-radius: 16px;
+  background: #fff;
+  margin: 16px 0;
+  height: 60px;
+
+  flex-direction: row;
+`
+
+export const DescriptionTodo = styled.TextInput`
+  padding-left: 32px;
+  flex: 1;
+`
+
+export const ButtonNewTodo = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+
+  border-radius: 16px;
+  background: #ff1744;
+
+  justify-content: center;
+  align-items: center;
 `
